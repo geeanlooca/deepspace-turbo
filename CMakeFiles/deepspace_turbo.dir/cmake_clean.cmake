897e03b67e6d2d3b97b80a522070c182aa6d13b4
Gianluca Marcon <marcon.gluca@gmail.com>
2017-02-20 21:39:33 +0100

    First commit

---
+++ CMakeFiles/deepspace_turbo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deepspace_turbo.dir/main.c.o"
+  "CMakeFiles/deepspace_turbo.dir/utilities.c.o"
+  "CMakeFiles/deepspace_turbo.dir/libconvcodes.c.o"
+  "deepspace_turbo.pdb"
+  "deepspace_turbo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/deepspace_turbo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
