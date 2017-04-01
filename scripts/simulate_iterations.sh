e5d4d3370f6107614b991d1cd1b6066d9c74e16f
Gianluca Marcon <marcon.gluca@gmail.com>
2017-04-01 14:28:39 +0200

    updated simulation script with higher packet count

--- scripts/simulate_iterations.sh
+++ scripts/simulate_iterations.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PKT_COUNT=100
-MIN_SNR=-2
+PKT_COUNT=10000
+MIN_SNR=-3
 MAX_SNR=1
 SNR_POINTS=10
-for i in 1 2 3
+for i in 1 2 3 5
 do
     FILENAME="${PKT_COUNT}pkts_${i}iter.csv"
     time ../bin/deepspace_turbo -y -m $MIN_SNR -M  $MAX_SNR -n $SNR_POINTS -i $i -o $FILENAME -c $PKT_COUNT
