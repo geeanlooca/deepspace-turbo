f5fb289070799cb3a36c061cba9e2d9e1060549f
Gianluca Marcon <marcon.gluca@gmail.com>
2017-04-03 00:01:08 +0200

    updated simulation script: results are stored in a simulation-specific folder, along with execution time

--- scripts/simulate_iterations.sh
+++ scripts/simulate_iterations.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-PKT_COUNT=10000
+# PKT_COUNT=10000
+PKT_COUNT=10
 MIN_SNR=-3
 MAX_SNR=1
 SNR_POINTS=10
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+# create results directory
+mkdir -p results
+
+# create directory for this specific simulation
+DIR="./results/$TIMESTAMP"
+mkdir -p $DIR
+
+# log file to save simulation time
+LOG="$DIR/time.log"
+
+# simulate with different number of iterations
 for i in 1 2 3 5
 do
-    FILENAME="${PKT_COUNT}pkts_${i}iter.csv"
-    time ../bin/deepspace_turbo -y -m $MIN_SNR -M  $MAX_SNR -n $SNR_POINTS -i $i -o $FILENAME -c $PKT_COUNT
+    # save results in the following file
+    FILENAME="$DIR/${PKT_COUNT}pkts_${i}iter.csv"
+
+    # run simulator
+    /usr/bin/time -f "%E" -a -o $LOG ../bin/deepspace_turbo -y -m $MIN_SNR -M  $MAX_SNR -n $SNR_POINTS -i $i -o $FILENAME -c $PKT_COUNT > /dev/null
+
 done
