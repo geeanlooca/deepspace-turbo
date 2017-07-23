da819b4270672c3e76a1ec571e27a729626ec4a3
Gianluca Marcon <marcon.gluca@gmail.com>
2017-07-23 03:03:48 +0200

    updated runner scripts

--- scripts/simulate_iterations.sh
+++ scripts/simulate_iterations.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-PKT_COUNT=1000
+
+if [ -z "$1" ]; then
+    PKT_COUNT=1000
+else
+    PKT_COUNT=$1
+fi
+
+if [ -z "$2" ]; then
+    CODE=1
+else
+    CODE=$2
+fi
+
+if [ -z "$3" ]; then
+    OCTETS=1
+else
+    OCTETS=$3
+fi
+
 MIN_SNR=0.5
-MAX_SNR=2.5
 SNR_POINTS=10
 CORES=4
-OCTETS=2
 TIMESTAMP=$(date +%F-%H-%M-%S)
+
+echo "CODE: ${CODE}"
+echo "PACKET COUNT: ${PKT_COUNT}"
+echo "OCTETS: ${OCTETS}"
 
 # create results directory
 mkdir -p results
@@ -19,10 +39,18 @@ mkdir -p $DIR
 # log file to save simulation time
 LOG="$DIR/time.log"
 
+echo "CODE: ${CODE}" >> $LOG
 echo "PACKET COUNT: ${PKT_COUNT}" >> $LOG
 echo "OCTETS: ${OCTETS}" >> $LOG
 echo "" >> $LOG
 echo "" >> $LOG
+
+declare -a MAX_SNR[7]
+MAX_SNR[1]=5
+MAX_SNR[2]=3
+MAX_SNR[3]=2.5
+MAX_SNR[5]=2
+MAX_SNR[7]=2
 
 PARAMS=""
 # simulate with different number of iterations
@@ -30,19 +58,20 @@ for i in 1 2 3 5 7
 do
 
     echo "Simulating with ${i} iterations..."
+    echo "Max SNR: ${MAX_SNR[i]} dB"
 
     # save results in the following file
     FILENAME="$DIR/${PKT_COUNT}pkts_${i}iter.csv"
     LABEL="${i} iterations"
 
     echo $FILENAME >> $LOG
-    echo "" >> $LOG
     
     PARAMS="$PARAMS $FILENAME \"${LABEL}\""
 
     # run simulator
-    /usr/bin/time -f "%E" -a -o $LOG ../bin/deepspace_turbo -y -m $MIN_SNR -M  $MAX_SNR -n $SNR_POINTS -i $i -o $FILENAME -c $PKT_COUNT -C $CORES -t 1 -k ${OCTETS}
+    /usr/bin/time -f "%E" -a -o $LOG ../bin/deepspace_turbo -y -m $MIN_SNR -M  ${MAX_SNR[i]} -n $SNR_POINTS -i $i -o $FILENAME -c $PKT_COUNT -C $CORES -t ${CODE} -k ${OCTETS}
     
+    echo "" >> $LOG
     echo "Done..."
 
 done
--- scripts/simulate_rates.sh
+++ scripts/simulate_rates.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-PKT_COUNT=10 
+
+if [ -z "$1" ]; then
+    PKT_COUNT=1000
+else
+    PKT_COUNT=$1
+fi
+
+if [ -z "$2" ]; then
+    ITER=3
+else
+    ITER=$2
+fi
+
+if [ -z "$3" ]; then
+    OCTETS=4
+else
+    OCTETS=$3
+fi
+
 MIN_SNR=0
 SNR_POINTS=10
 TIMESTAMP=$(date +%F-%H-%M-%S)
-OCTETS=4
 CORES=4
-ITER=3
 
 # create results directory
 mkdir -p results
@@ -25,16 +41,17 @@ echo "OCTETS: ${OCTETS}" >> $LOG
 echo "" >> $LOG
 echo "" >> $LOG
 
-
+declare -a LABERARRAY[3]
 LABELARRAY[0]='Rate 1/2'
 LABELARRAY[1]='Rate 1/3'
 LABELARRAY[2]='Rate 1/4'
 LABELARRAY[3]='Rate 1/6'
 
-MAX_SNR[1]=2
-MAX_SNR[2]=1.5
-MAX_SNR[3]=1.1
-MAX_SNR[4]=1
+declare -a MAX_SNR[3]
+MAX_SNR[0]=2
+MAX_SNR[1]=1.5
+MAX_SNR[2]=1.1
+MAX_SNR[3]=1
 
 PARAMS=""
 for i in  1 2 3 4
@@ -44,16 +61,16 @@ do
     FILENAME="$DIR/${PKT_COUNT}pkts_code${i}.csv"
 
     echo $FILENAME >> $LOG
+    echo "" >> $LOG
     
     LABEL=${LABELARRAY[i-1]}
     PARAMS="$PARAMS $FILENAME \"${LABEL}\""
 
     # run simulator
-    /usr/bin/time -f "%E" -a -o $LOG ../bin/deepspace_turbo -y -m $MIN_SNR -M  ${MAX_SNR[i]} -n $SNR_POINTS -i $ITER -o $FILENAME -c $PKT_COUNT -k ${OCTETS} -C ${CORES} -t ${i}
+    /usr/bin/time -f "%E" -a -o $LOG ../bin/deepspace_turbo -y -m $MIN_SNR -M  ${MAX_SNR[i-1]} -n $SNR_POINTS -i $ITER -o $FILENAME -c $PKT_COUNT -k ${OCTETS} -C ${CORES} -t ${i}
 
     echo "Done."
     echo ""
-    echo "" >> $LOG
 done
 
 PARAMS="$PARAMS $DIR/rate_plot.png"
